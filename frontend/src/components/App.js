f74d3d1ea1acd89ce7a835468542b6393a3f1a72
Rick N <richard.norman.id@gmail.com>
2022-04-06 13:39:56 -0600

    initial setup of react router and pages complete

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,6 +5,20 @@ import { RealmAppProvider, useRealmApp } from "./RealmApp";
 import { ThemeProvider } from "./Theme";
 import { AppName } from "./AppName";
 import { appId } from "../realm.json";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import AccountPage from '../pages/Account/AccountPage';
+import AllCoursesPage from '../pages/AllCourses/AllCoursesPage';
+import ChatPage from '../pages/Chat/ChatPage';
+import CourseModulesPage from '../pages/CourseModules/CourseModulesPage';
+import HomePage from '../pages/Home/HomePage';
+import InterestsPage from '../pages/Interests/InterestsPage';
+import LearningLogPage from '../pages/LearningLog/LearningLogPage';
+import ModulePage from '../pages/Module/ModulePage';
+import ModuleQuizPage from '../pages/ModuleQuiz/ModuleQuizPage';
 import "./App.css";
 
 export default function AppWithRealm() {
@@ -20,24 +34,64 @@ export default function AppWithRealm() {
 function App() {
   const { currentUser, logOut } = useRealmApp();
   return (
-    <div className="App">
-      <AppBar position="sticky">
-        <Toolbar>
-          <AppName />
-          {currentUser ? (
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={async () => {
-                await logOut();
-              }}
-            >
-              <Typography variant="button">Log Out</Typography>
-            </Button>
-          ) : null}
-        </Toolbar>
-      </AppBar>
-      {currentUser ? <TodoItemsPage /> : <WelcomePage />}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/learnlylog'>
+          <HomePage />
+        </Route>
+        <Route path='/learnlylog/chat'>
+          <ChatPage />
+        </Route>
+        <Route path='/learnlylog/course-modules'>
+          <CourseModulesPage />
+        </Route>
+        <Route path='/learnlylog/module-quiz'>
+          <ModuleQuizPage />
+        </Route>
+        <Route path='/learnlylog/account'>
+          <AccountPage />
+        </Route>
+        <Route exact path='/learnlylog/all-courses'>
+          <AllCoursesPage />
+        </Route>
+        <Route path='/learnlylog/interests'>
+          <InterestsPage />
+        </Route>
+        <Route path='/learnlylog/module'>
+          <ModulePage />
+        </Route>
+        <Route path='/learnlylog/sign-in'>
+          <TodoItemsPage />
+        </Route>
+        <Route path='/learnlylog/register'>
+          <WelcomePage />
+        </Route>
+        <Route path='/learnlylog/log'>
+          <LearningLogPage />
+        </Route>
+      </Switch>
+    </Router>
+    //<div className="App">
+    //  <AppBar position="sticky">
+    //    <Toolbar>
+    //      <AppName />
+    //      {currentUser ? (
+    //        <Button
+    //          variant="contained"
+    //          color="secondary"
+    //          onClick={async () => {
+    //            await logOut();
+    //          }}
+    //        >
+    //          <Typography variant="button">Log Out</Typography>
+    //        </Button>
+    //      ) : null}
+    //    </Toolbar>
+    //  </AppBar>
+    //  {currentUser ? <TodoItemsPage /> : 
+    //    <Link to="/home">Home</Link>
+    //  }
+//
+   // </div>
   );
 }
