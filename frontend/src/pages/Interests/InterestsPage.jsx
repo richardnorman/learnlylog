587790575a603abec413780ba70b29b8caa3b473
Suhaib Tariq <43761914+suhaib99@users.noreply.github.com>
2022-04-11 02:52:44 -0600

    Complete Module and Interest page UI

--- frontend/src/pages/Interests/InterestsPage.jsx
+++ frontend/src/pages/Interests/InterestsPage.jsx
@@ -1,7 +1,162 @@
 import * as React from "react";
+import "./InterestsPageStyle.css";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+//from '@material-ui/icons';
+//import Typography from '@material-ui/core/Typography';
+import { CardActionArea, Container } from '@material-ui/core';
+//import PropTypes from 'prop-types';
+//import LinearProgress from '@material-ui/core/LinearProgress';
+//import Box from '@material-ui/core/Box';
+//import Fab from '@material-ui/core/Fab';
+
+
+let interests = [
+    {
+        InterestName: "Technology",
+        InterestImage: "https://cdn-icons.flaticon.com/png/512/4365/premium/4365271.png?token=exp=1649655256~hmac=7b55893d58520ddea02e5632d9ec4a48"
+    },
+    {
+        InterestName: "UX/UI",
+        InterestImage: "https://cdn-icons.flaticon.com/png/512/1367/premium/1367672.png?token=exp=1649667176~hmac=ff499f00476b20b001d4fd429dc58f86"
+    },
+    {
+        InterestName: "Game Design",
+        InterestImage: "https://cdn-icons-png.flaticon.com/512/141/141073.png"
+    },
+    {
+        InterestName: "Music",
+        InterestImage: "https://cdn-icons-png.flaticon.com/512/4472/4472592.png"
+    },
+    {
+        InterestName: "Cooking",
+        InterestImage: "https://cdn-icons-png.flaticon.com/512/1689/1689198.png"
+    },
+    {
+        InterestName: "Web Development",
+        InterestImage: "https://cdn-icons.flaticon.com/png/512/2828/premium/2828990.png?token=exp=1649667287~hmac=c2dcf80fb43923c3c9c0f5e267c38889"
+    }
+]
+
+
+
+
+function InterestCard() {
+    return (
+        <div className="CardsContainer">
+        <Card style={{ height: 240, margin: 5, width: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons.flaticon.com/png/512/4365/premium/4365271.png?token=exp=1649655256~hmac=7b55893d58520ddea02e5632d9ec4a48"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>Technology</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+       <Card style={{ height: 240, margin: 5, maxWidth: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons.flaticon.com/png/512/1367/premium/1367672.png?token=exp=1649667176~hmac=ff499f00476b20b001d4fd429dc58f86"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>UX/UI</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+       <Card style={{ height: 240, margin: 5, maxWidth: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons-png.flaticon.com/512/141/141073.png"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>Game Design</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+       <Card style={{ height: 240, margin: 5, maxWidth: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons-png.flaticon.com/512/4472/4472592.png"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>Music</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+       <Card style={{ height: 240, margin: 5, maxWidth: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons-png.flaticon.com/512/1689/1689198.png"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>Cooking</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+       <Card style={{ height: 240, margin: 5, maxWidth: 200, backgroundColor: '#F1F3F4' }} variant="outlined">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="180"
+                    image="https://cdn-icons.flaticon.com/png/512/2828/premium/2828990.png?token=exp=1649667287~hmac=c2dcf80fb43923c3c9c0f5e267c38889"
+                />
+                <CardContent
+                    height="50"
+                >
+                    <p style={{ textAlign: 'center' }}>Web Development</p>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+       {/* -------------------------------------------------------------------------------------- */}
+
+
+        </div>
+
+    );
+}
+
+
 
 export default function InterestsPage() {
     return (
-        <h1>Interests</h1>
+        <nav>
+            <div className="h2container">
+                <h1 className="InterestPageHeading"> Choose your interests</h1>
+            </div>
+            <InterestCard />
+        </nav>
+
     );
 }
--- frontend/src/pages/Module/ModulePage.jsx
+++ frontend/src/pages/Module/ModulePage.jsx
@@ -1,7 +1,106 @@
 import * as React from "react";
+import ReactDOM from "react-dom"
+import { AppBar, Toolbar, Button, Typography, IconButton, ButtonBase, Fab } from "@material-ui/core";
+import { AccountCircle, CalendarToday, ExitToApp, Chat } from '@material-ui/icons';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "./ModulePage.css";
+
+let moduleData = {
+    "title": "Module",
+    "properties": {
+        "_id": {
+            "bsonType": "objectId"
+        },
+        "courseId": {
+            "bsonType": "objectId"
+        },
+        "description": {
+            "bsonType": "This is module's description"
+        },
+        "name": {
+            "bsonType": "Module Name"
+        },
+        "content": {
+            "bsonType": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        }
+    }
+}
+
+let moduleName = moduleData.properties.name.bsonType
+let moduleDescription = moduleData.properties.description.bsonType
+let moduleContent = moduleData.properties.content.bsonType
+
+console.log(moduleName);
+
+function ModulePageHead(){
+    return(
+        <div className="modulePageHeadStyle">
+            <h1>{moduleName}</h1>
+        </div>
+        
+    );
+};
+
+function ModulePageDescription(){
+    return(
+        <h3>{moduleDescription}</h3>
+    );
+};
+
+function ModulePageContent(){
+    return(
+        <p>{moduleContent}</p>
+    );
+};
+
+
+// function Testing() {
+//     //let display =content1.properties.content;
+//     return <h1>test</h1>;
+// }
+const takeQuizButtonStyle = {
+    borderRadius: 50,
+    padding :25,
+    margin: 30,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+};
+
+
+function TakeQuizButton() {
+
+    const navigate = useNavigate();
+
+    const redirect = () => {
+    navigate('/ModuleQuiz/ModuleQuizPage.jsx')
+    }
+
+    return (
+        <Fab className="takeQuizButton" 
+            size="large"
+            color="primary"
+            variant="contained"
+            style={takeQuizButtonStyle}
+            onClick={redirect}
+            >
+            Take Quiz
+        </Fab>
+    );
+};
+
 
 export default function ModulePage() {
     return (
-        <h1>Module</h1>
+        <nav>
+            <ModulePageHead />
+            <ModulePageDescription />
+            <ModulePageContent />
+            <TakeQuizButton />
+        </nav>
     );
 }
+
+// ReactDOM.render(<ModulePage />, document.getElementById("root") )
