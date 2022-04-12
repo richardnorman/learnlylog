00322d284cd5d4beb76522478441565c9e51a9cc
Bennett Gunson <bengunson@gmail.com>
2022-04-12 14:26:19 -0600

    add welcome message for userrs with no enrolled courses

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -3,8 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { CardActionArea, Container, CircularProgress } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import { CardActionArea, Container, CircularProgress, Chip } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Box from '@material-ui/core/Box';
 import Fab from '@material-ui/core/Fab';
@@ -14,7 +13,7 @@ import './HomePage.css';
 import { useCourseEnrollment } from '../../hooks/useCourseEnrollment';
 import { useModules } from '../../hooks/useModules';
 import { useUserModules } from '../../hooks/useUserModules';
-
+import { useRealmApp } from "../../components/RealmApp";
 
 
 const DisplayCourses = props => {
@@ -30,6 +29,8 @@ const DisplayCourses = props => {
             let attempts = cModules.map(m => getAttempt(m._id));    // get attempts for each module, if any
             progress = Math.round((attempts.filter(a => a && a.bestScore >= 75).length / cModules.length) * 100);
         }
+
+        if (isNaN(progress)) progress = 0;  // something wack here
 
         return (
             <Link to={`course-modules/${course._id}`} className="course-card" style={{ textDecoration: "none" }}>
@@ -66,18 +67,38 @@ const DisplayCourses = props => {
 }
 
 export default function HomePage() {
+
+    const realmApp = useRealmApp();
+
     const { courses, loading } = useCourses();
     const { isEnrolled } = useCourseEnrollment();
 
+    const myCourses = () => courses.filter(c => isEnrolled(c._id));
+
     return loading ?
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <CircularProgress style={{marginTop: "10%"}}/>
+            <CircularProgress style={{ marginTop: "10%" }} />
         </div>
         : (
             <div className="page-content">
                 <h2>My Courses</h2>
                 <div className='my-courses-content'>
-                    <DisplayCourses courses={courses.filter(c => isEnrolled(c._id))} isEnrolled={true} />
+                    {
+                        !myCourses().length ?
+                            <div style={{ textAlign: "center", width: "100%" }}>
+                                <Chip variant="outlined" color="primary" label={
+                                        <Typography variant="h5" component="div">
+                                            {`Welcome, ${realmApp.currentUser.profile.email.split('@')[0]}!`}
+                                        </Typography>
+                                    }>
+                                </Chip>
+                                <Typography variant="subtitle1" gutterBottom component="div">
+                                    {`We've listed some courses below that we think you might enjoy and take something meaningful from. Click one to view its required modules and enroll when you're ready!`}
+                                </Typography>
+                            </div>
+                            :
+                            <DisplayCourses courses={myCourses()} isEnrolled={true} />
+                    }
                 </div>
                 <div id='all-courses-heading-content'>
                     <h2>All Courses</h2>
